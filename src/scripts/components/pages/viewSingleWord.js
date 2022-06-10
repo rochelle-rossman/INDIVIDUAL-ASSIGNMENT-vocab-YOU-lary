91e9999106a18b02ecee004cafcf19493aac15eb
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-06-10 11:11:38 -0500

    completed filtering function

---
+++ src/scripts/components/pages/viewSingleWord.js
@@ -0,0 +1,18 @@
+import clearDom from '../../helpers/clearDom';
+import renderToDOM from '../../helpers/renderToDom';
+
+const viewSingleWord = (obj) => {
+  clearDom();
+  const domString = `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h3 class="card-title">${obj.title}</h3>
+        <p class="card-text"><b>Definition:</b> ${obj.definition}</p>
+        <p class="card-text"><b>Language/Tech:</b> ${obj.language}</p>
+        <i class="fas fa-edit btn btn-info" id="edit-word--${obj.firebaseKey}"></i>
+        <i class="btn btn-danger fas fa-trash-alt" id="delete-word-btn--${obj.firebaseKey}"></i>
+      </div>
+    </div>`;
+  renderToDOM('#cards', domString);
+};
+
+export default viewSingleWord;
--- src/scripts/helpers/auth/startApp.js
+++ src/scripts/helpers/auth/startApp.js
@@ -2,15 +2,17 @@ import { getWords } from '../../../api/vocabData';
 import logoutButton from '../../components/buttons/logoutButton';
 import domBuilder from '../../components/domBuilder';
 import navBar from '../../components/navBar';
-import { showVocabCards } from '../../components/pages/vocabCards';
+import { showVocabCards } from '../../components/pages/showVocabCards';
 import domEvents from '../../events/domEvents';
 import formEvents from '../../events/formEvents';
+import navEvents from '../../events/navEvents';
 
 const startApp = (user) => {
   domBuilder();
   domEvents(user.uid);
   formEvents(user.uid);
   navBar();
+  navEvents(user.uid);
   logoutButton();
 
   getWords(user.uid).then((wordsArray) => showVocabCards(wordsArray));
